5d3ae1c8d5a0ed645f6f37f86292770b11e337c4
saambpatil@outlook.com <saambpatil@outlook.com>
2024-08-24 15:05:39 +0530

    posts

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+
 class Post(BaseModel):
-    title:str
-    content:str
-    
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get("/")
 def root():
     return {"message": "Hello!!!! "}
+
 
 @app.get("/posts")
 def get_post():
@@ -23,8 +28,9 @@ def get_post():
 #     print(temp)
 #     return {"new_post": f"title {temp['title']} content: {temp['content']}"}
 
+
 @app.post("/createposts")
-def create_post(new_post : Post):
-    print(new_post)
-    # return {"new_post": f"title {temp['title']} content: {temp['content']}"}
-    return {"data": f"{new_post.title}"}
+def create_post(post: Post):
+    # print(new_post)
+    print(post.dict())
+    return {"data": f"{post.title}"}
